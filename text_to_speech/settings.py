76b5552effe5926bb5dfee88aab6c5b6fa49c401
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2024-04-09 11:40:53 +0300

    update settings.py

--- text_to_speech/settings.py
+++ text_to_speech/settings.py
@@ -282,17 +282,13 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = (
     BASE_DIR / 'staticfiles',
 )
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
