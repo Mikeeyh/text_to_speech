9074e3c734d3bc1609669fee511e00d9145c5fa3
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2024-04-10 10:57:24 +0300

    up

--- text_to_speech/settings.py
+++ text_to_speech/settings.py
@@ -186,7 +186,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-uv#+6+l6zp9hb#w7i5!o&dmktd2w_m^zm$nrl0mc!lx@lxy3)4'
 
 
-DEBUG = os.environ.get("DEBUG", 1)
+DEBUG = os.environ.get("DEBUG", "1") == "1"
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost").split(" ")
 CSRF_TRUSTED_ORIGINS = [f"https://{host}" for host in ALLOWED_HOSTS]
