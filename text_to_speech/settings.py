1ad74734704e61aecd5d12ab3b9878429d27ec9a
Mikeeyh <133797823+Mikeeyh@users.noreply.github.com>
2024-04-10 15:55:34 +0300

    up

--- text_to_speech/settings.py
+++ text_to_speech/settings.py
@@ -13,7 +13,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-uv#+6+l6zp9hb#w7i5!o&dmktd2w_m^zm$nrl0mc!lx@lxy3)4'
 
-
 DEBUG = os.environ.get("DEBUG", 1)
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost").split(" ")
